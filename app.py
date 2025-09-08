0d237f1d6cfc3429b3fcf8bb0c91f72c43179829
vishwanath-r-460 <vishwauchiha@outlook.com>
2025-09-08 23:00:57 +0530

    updated requirements and app.py for eb deployment

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
 # Database setup (SQLite file: blog.db)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+if os.environ.get("EB_ENV"):  # Running in Elastic Beanstalk
+    db_path = "/tmp/blog.db"
+else:  # Running locally
+    db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "blog.db")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -17,7 +23,7 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-# Home page - show all posts
+# Routes
 @app.route('/')
 def home():
     posts = Post.query.order_by(Post.created_at.desc()).all()
@@ -31,7 +37,6 @@ def about():
 def contact():
     return render_template("contact.html")
 
-# Create post
 @app.route('/create', methods=["GET", "POST"])
 def create():
     if request.method == "POST":
@@ -44,13 +49,11 @@ def create():
             return redirect(url_for("home"))
     return render_template("create.html")
 
-# View single post
 @app.route('/post/<int:post_id>')
 def post_detail(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template("post_detail.html", post=post)
 
-# Edit post
 @app.route('/edit/<int:post_id>', methods=["GET", "POST"])
 def edit(post_id):
     post = Post.query.get_or_404(post_id)
@@ -61,7 +64,6 @@ def edit(post_id):
         return redirect(url_for("post_detail", post_id=post.id))
     return render_template("edit.html", post=post)
 
-# Delete post
 @app.route('/delete/<int:post_id>', methods=["POST"])
 def delete(post_id):
     post = Post.query.get_or_404(post_id)
@@ -69,7 +71,11 @@ def delete(post_id):
     db.session.commit()
     return redirect(url_for("home"))
 
+# For Elastic Beanstalk WSGI
+application = app
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
